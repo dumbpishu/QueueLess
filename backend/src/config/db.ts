142521e1c217e569ee2468cca0c2eae698761a81
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-02-06 19:16:31 +0530

    connect with database locally successfully

---
+++ backend/src/config/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI;
+        if (!MONGO_URI) {
+            throw new Error("MONGO_URI is not defined in .env")
+        }
+
+        const connIns = await mongoose.connect(MONGO_URI);
+        console.log(`MongoDB!! Connected Successfully. HOST: ${connIns.connection.host}`)
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
