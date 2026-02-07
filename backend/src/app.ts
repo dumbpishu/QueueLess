bc05d613948b847f00c30289eafda041df802336
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-02-07 19:14:06 +0530

    connect with database

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true,
+}
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import app from "./app";
+import connectDB from "./config/db";
+
+const PORT = process.env.PORT || 8000;
+
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    })
+}
+
+startServer();
