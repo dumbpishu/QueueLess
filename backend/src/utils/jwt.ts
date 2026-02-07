ec9c3d2549b04f186fc36f25d40830e7f4bfe64b
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-02-07 17:56:10 +0530

    remove backend

--- backend/src/utils/jwt.ts
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const AUTH_TOKEN_SECRET = process.env.AUTH_TOKEN_SECRET!;
-
-export const generateAuthToken = (userId: string) => {
-  return jwt.sign({ userId }, AUTH_TOKEN_SECRET, { expiresIn: "7d" });
-};
